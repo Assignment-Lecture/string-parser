343975fc7a043359153a504a3fa69654c35ab8da
Jonny-Cho <andante2183@gmail.com>
2022-04-10 15:08:36 +0900

    setting: 프로젝트 세팅

---
+++ src/main/java/com/stringparser/StringParserApplication.java
@@ -0,0 +1,13 @@
+package com.stringparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringParserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StringParserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/stringparser/StringParserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stringparser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StringParserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
